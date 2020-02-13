14a0b6019080f2f4766e490d9374eb59aa80f7b0
Maciek <dobrytyp@gmail.com>
2020-02-13 20:08:00 +0100

    13-02-2020

--- 08-02-2020.py
+++ 08-02-2020.py
@@ -7,13 +7,15 @@
 # lista = [89, 30, 52, 58, 73, 22, 77, 85, 21, 70, 22, 43, 53, 39, 26, 56, 24, 42, 49]
 #
 # lista2 = [34, 15, 19, 7, 79, 74, 66, 25, 48, 83, 87, 82, 32, 77, 70, 50, 74, 63, 1]
-
+#
 # lista3 = []
 # for i in lista:
 #     if i in lista2:
 #         lista3.append(i)
-#
+
 # print(lista3)
+
+#---------------------------------------------------------
 
 # def common_elements(lista, lista2):
 #     common_elem = []
@@ -24,7 +26,7 @@
 #
 # print(common_elements([1,3,5,6,9,11], [2,3,5,23,44]))
 
-# Zwiększamy wydajność
+# Zwiększamy wydajność -----------------------------
 
 # def common_elements(lista, lista2):
 #     common_elem = []
@@ -36,7 +38,7 @@
 #
 # print(common_elements([1,3,5,6,9,11], [2,3,5,23,44]))
 
-# Napisz program znajdujący tak podział listy, by wartość bezwzględna sum elementów po loewej stronie i elementów
+# Napisz program znajdujący tak podział listy, by wartość bezwzględna sum elementów po lewej stronie i elementów
 # po prawej była jak najmniejsza. Zwróc tę wartość. Okreś złożóność programu
 
 # def get_min_diff(lst):
@@ -69,7 +71,7 @@
 # Napisz program sumujący elementy ciągu powstałego w wyniku użycia funkcji range. Okreś jeso zlożoność
 
 
-# lista = list(range(0, 20, 3)) # To jest stałyt element
+# lista = list(range(0, 20, 3)) # To jest stały element
 #
 # def get_sum(lista):
 #     suma = 0
@@ -78,20 +80,24 @@
 #     return suma
 #
 # print(get_sum(lista))
-#
+
 # # Albo
+# lista = list(range(0, 20, 3)) # To jest stały element
 #
 # def get_sum2(lista):
 #     return sum(lista)
 #
 # print(get_sum2(lista))
-#
+
 # # Albo Najlepiej
+# lista = list(range(0, 20, 3)) # To jest stały element
 #
 # def get_sum3(lista):
 #     return (lista[0] + lista[-1]) / 2 * len(lista)
 #
 # print(get_sum3(lista))
+
+#=================================================
 
 # lista = [5,8,1,4,9,4]
 #
