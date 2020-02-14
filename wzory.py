2c170ed5c8d5a21c095ed86368ab895547711195
Dobrytyp <58483489+Dobrytyp@users.noreply.github.com>
2020-02-14 13:10:56 +0100

    Create wzory.py

---
+++ wzory.py
@@ -0,0 +1,47 @@
+# #Pierwsze
+
+# pierwsze = []
+
+# def prime_in_range(n):
+#     if n > 1:
+#         for i in range(2, n-1):
+#             if n % i == 0:
+#                 break
+#         else:
+#             pierwsze.append(n)
+#         prime_in_range(n-1)
+#     else:
+#         print(pierwsze)
+        
+# print(prime_in_range(100))
+
+#---------------------------------------------------------------------
+
+# fibonacci = []
+
+# def fibbonacci_string(n):
+#     if n == 1:
+#         return 0 
+#     elif n == 2:
+#         return 1
+#     else:
+#         return fibbonacci_string(n-1) + fibbonacci_string(n-2)
+    
+# print(fibbonacci_string(9))
+
+#-----------------------------------------------------------------
+
+# silnia
+
+# def silnia(n):
+#     if n == 0:
+#         return 1
+#     elif n == 1:
+#         return 2
+#     else:
+#         wynik = 1
+#         for i in range(1, n+1):
+#             wynik *= i
+#         return wynik
+
+# print(silnia(6))
